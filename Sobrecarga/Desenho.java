db2db1756cf9e7bd13e455c8f6ca8a00ee7478bc
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-09-18 12:02:08 -0300

    Create Desenho.java

---
+++ Sobrecarga/Desenho.java
@@ -0,0 +1,28 @@
+/**
+ * Classe criada para criar objetos do tipo Figura e executar métodos 
+ * sobrecarregados
+ * @author Pedro Pereira Guaita
+ * @since Classe criada em 18/09/2025
+ */
+public class Desenho {
+    public static void main(String[] args) {
+      
+      /*
+      Figura x = new Figura(500); //Criando um ponto
+      Figura y = new Figura(10, 70); //Criando uma Linha
+      Figura z = new Figura(10, 20, 30); //Criando um Triângulo
+      Figura r = new Figura(10, 30, 50, 70); //Criando um Retângulo
+      
+      
+      x.exibirFigura();
+      y.exibirFigura();
+      z.exibirFigura();
+      r.exibirFigura();
+      */
+      
+      Menu a = new Menu();
+      
+      
+    }//fim do main
+
+}//fim da classe
